506b9bfa2d37c1020b7e6a3258dfd70f6a9b97ca
TheLastGimbus <mateusz.soszynski.2003@gmail.com>
2019-12-27 00:06:27 +0100

    Starting camera and passing it to viewFinder

--- app/src/main/java/com/example/dicerollserver/MainActivity.kt
+++ app/src/main/java/com/example/dicerollserver/MainActivity.kt
@@ -2,9 +2,15 @@ package com.example.dicerollserver
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Matrix
 import android.os.Bundle
+import android.view.Surface
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -39,11 +45,42 @@ class MainActivity : AppCompatActivity() {
     private val executor = Executors.newSingleThreadExecutor()
 
     private fun startCamera() {
-        // TODO: Implement CameraX operations
+        // Create configuration object for the viewfinder use case
+        val previewConfig = PreviewConfig.Builder().build()
+
+        val preview = Preview(previewConfig)
+        preview.setOnPreviewOutputUpdateListener {
+            // To update the SurfaceTexture, we have to remove it and re-add it
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+
+            viewFinder.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+
+        CameraX.bindToLifecycle(this, preview)
     }
 
     private fun updateTransform() {
-        // TODO: Implement camera viewfinder transformations
+        val matrix = Matrix()
+
+        // Compute the center of the view finder
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+        // Correct preview output to account for display rotation
+        val rotationDegrees = when (viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+
+        // Finally, apply transformations to our TextureView
+        viewFinder.setTransform(matrix)
     }
 
     override fun onRequestPermissionsResult(
