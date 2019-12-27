5f395385e1244c23a8d353f90c95ad88b1a3c66c
TheLastGimbus <mateusz.soszynski.2003@gmail.com>
2019-12-27 18:44:53 +0100

    Wifi lock

--- app/src/main/java/com/example/dicerollserver/ServerForegroundService.kt
+++ app/src/main/java/com/example/dicerollserver/ServerForegroundService.kt
@@ -1,10 +1,15 @@
 package com.example.dicerollserver
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.net.wifi.WifiManager
 import android.os.IBinder
 
+
 class ServerForegroundService : Service() {
+    private lateinit var wifiLock: WifiManager.WifiLock
+    private val WIFI_LOCK_TAG = "server_wifi_lock"
 
     companion object {
         const val TAG = "ServerService"
@@ -20,5 +25,18 @@ class ServerForegroundService : Service() {
             Notifs.Id.FOREGROUND_SERVICE,
             Notifs.getForegroundNotification(this)
         )
+
+        // Acquire wifi lock to keep wifi alive to handle request to server
+        val wm = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        wifiLock = wm.createWifiLock(WifiManager.WIFI_MODE_FULL_HIGH_PERF, WIFI_LOCK_TAG)
+        wifiLock.acquire()
+
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (wifiLock.isHeld) wifiLock.release()
     }
 }
