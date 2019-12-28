c5cf4eb38b5756debd4706f66ab498193f8cb7e8
TheLastGimbus <mateusz.soszynski.2003@gmail.com>
2019-12-28 15:55:25 +0100

    Added WebServer to service

--- app/src/main/java/com/example/dicerollserver/ServerForegroundService.kt
+++ app/src/main/java/com/example/dicerollserver/ServerForegroundService.kt
@@ -5,15 +5,20 @@ import android.content.Context
 import android.content.Intent
 import android.net.wifi.WifiManager
 import android.os.IBinder
+import android.util.Log
+import fi.iki.elonen.NanoHTTPD
 
 
 class ServerForegroundService : Service() {
-    private lateinit var wifiLock: WifiManager.WifiLock
-    private val WIFI_LOCK_TAG = "server_wifi_lock"
 
     companion object {
         const val TAG = "ServerService"
     }
+
+    private lateinit var wifiLock: WifiManager.WifiLock
+    private val WIFI_LOCK_TAG = "server_wifi_lock"
+
+    private val server = WebServer(8822)
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
@@ -32,11 +37,31 @@ class ServerForegroundService : Service() {
         wifiLock.acquire()
 
 
-
+        server.start()
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        server.stop()
         if (wifiLock.isHeld) wifiLock.release()
+    }
+
+    private inner class WebServer(PORT: Int) : NanoHTTPD(PORT) {
+
+        override fun serve(session: IHTTPSession?): Response {
+            val res: Response = newFixedLengthResponse("Hello there!")
+            res.mimeType = "text/plain"
+            return res
+        }
+
+        override fun start() {
+            super.start()
+            Log.i(TAG, "Server started")
+        }
+
+        override fun stop() {
+            super.stop()
+            Log.i(TAG, "Server stopped")
+        }
     }
 }
